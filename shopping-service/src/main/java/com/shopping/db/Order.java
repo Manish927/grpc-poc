3af0274867d5392ca2d330a21eee95f3b7d87665
Manish Srivastava <mannu927@yahoo.com>
2021-09-13 12:52:44 +0530

    Update Order.java

--- shopping-service/src/main/java/com/shopping/db/Order.java
+++ shopping-service/src/main/java/com/shopping/db/Order.java
@@ -1,1 +1,52 @@
+package com.shopping.db;
+
+import java.util.Date;
+
+public class Order {
+    private int userId;
+    private int orderId;
+    private int numOfItems;
+    private double totalAmount;
+    private Date orderDate;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getNumOfItems() {
+        return numOfItems;
+    }
+
+    public void setNumOfItems(int numOfItems) {
+        this.numOfItems = numOfItems;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+}
 
